2e8ae5067fa917f21853ea9cd445271e2212b8e0
Saksham Chitkara <sakshamchitkara03@gmail.com>
2025-07-03 00:19:41 +0530

    add error handling

---
+++ error_handling.py
@@ -0,0 +1,21 @@
+def incomplete_arguments(cmd_name: str):
+    print(f"{cmd_name} : Incomplete Arguments")
+
+
+def invalid_arguments(cmd_name: str, attr_name: str, options: list):
+    print(f"{cmd_name}:{attr_name} : Invalid Argument > {options}")
+
+
+def compare_list(length: int, ideal: int, cmd_name: str):
+    if length < ideal:
+        incomplete_arguments(cmd_name)
+        return True
+    return False
+
+
+def db_error(err_msg: str):
+    print(f"Database Error: {err_msg}")
+
+
+def permission_error(cmd_name: str):
+    print(f"{cmd_name} : Permission Denied")
